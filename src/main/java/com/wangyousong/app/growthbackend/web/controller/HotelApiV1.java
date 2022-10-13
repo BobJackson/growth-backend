e55b9c2271fb8bd4b38c7f7374932829716dac6f
Bob Jackson <1187688895@qq.com>
2022-10-13 17:05:02 +0800

    add hotels api

---
+++ src/main/java/com/wangyousong/app/growthbackend/web/controller/HotelApiV1.java
@@ -0,0 +1,32 @@
+package com.wangyousong.app.growthbackend.web.controller;
+
+import com.wangyousong.app.growthbackend.common.R;
+import com.wangyousong.app.growthbackend.repository.mongo.HotelRepository;
+import com.wangyousong.app.growthbackend.web.controller.dto.HotelDtoV1;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/hotels")
+public class HotelApiV1 {
+
+    @Resource
+    private HotelRepository hotelRepository;
+
+    @Cacheable(cacheNames = {"hotelList"})
+    @CrossOrigin
+    @GetMapping
+    public R<List<HotelDtoV1>> list() {
+        List<HotelDtoV1> data = hotelRepository.findAll()
+                .stream()
+                .map(it -> new HotelDtoV1(it.getId(), it.getName(), it.getPictureUrl()))
+                .toList();
+        return R.success(data);
+    }
+}
---
+++ src/main/java/com/wangyousong/app/growthbackend/web/controller/dto/HotelDtoV1.java
@@ -0,0 +1,4 @@
+package com.wangyousong.app.growthbackend.web.controller.dto;
+
+public record HotelDtoV1(String id, String name, String picUrl) {
+}
