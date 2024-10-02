e6422a5bc86a6e87cfab0a9be800f15e82e2bec0
Bob Jackson <wangyousong@9191offer.com>
2024-10-02 16:11:28 +0800

    add @CrossOrigin for trello

--- src/main/java/com/wangyousong/app/growthbackend/web/controller/TrelloApiV1.java
+++ src/main/java/com/wangyousong/app/growthbackend/web/controller/TrelloApiV1.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
+@CrossOrigin
 public class TrelloApiV1 {
 
     private final TrelloListService service;
