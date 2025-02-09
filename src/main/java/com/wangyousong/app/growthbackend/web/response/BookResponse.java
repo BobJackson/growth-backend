848c072b558100a162cfcf6af7a37d82efba0435
Bob Jackson <wangyousong@9191offer.com>
2025-02-09 14:01:16 +0800

    refactor(book): 将 BookResponse 中的 press 字段重命名为 publisher
    - 将 BookResponse 类中的 press 字段重命名为 publisher，以更准确地反映书籍出版商的信息
    - 更新了相关的 getter 和 setter 方法
    - 修改了构造函数中的字段赋值

--- src/main/java/com/wangyousong/app/growthbackend/web/response/BookResponse.java
+++ src/main/java/com/wangyousong/app/growthbackend/web/response/BookResponse.java
@@ -21,7 +21,7 @@ public class BookResponse {
     private String category;
     private List<String> authors;
     private List<String> tags;
-    private String press;
+    private String publisher;
     private boolean hidden;
 
 
@@ -36,7 +36,7 @@ public class BookResponse {
         this.authors = book.getAuthors().stream().map(Author::getName).toList();
         Collection<Tag> tagList = ObjectUtils.defaultIfNull(book.getTags(), Collections.emptyList());
         this.tags = tagList.stream().map(Tag::getName).toList();
-        this.press = book.getPublisher();
+        this.publisher = book.getPublisher();
         this.hidden = book.isHidden();
     }
 }
